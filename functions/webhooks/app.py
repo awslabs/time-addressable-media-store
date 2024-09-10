6b94b0d1fa00ef86790e97b6ddbfa0922770db48
Dan Johns <danjhd@amazon.com>
2024-09-10 14:37:16 +0100

    added re-try logic to webhooks send for resiliency

--- functions/webhooks/app.py
+++ functions/webhooks/app.py
@@ -1,13 +1,14 @@
 import os
 
 import boto3
-import requests
 from aws_lambda_powertools import Logger, Metrics, Tracer
 from aws_lambda_powertools.utilities.data_classes.event_bridge_event import (
     EventBridgeEvent,
 )
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from boto3.dynamodb.conditions import Key
+from requests import Session
+from requests.adapters import HTTPAdapter, Retry
 
 tracer = Tracer()
 logger = Logger()
@@ -33,19 +34,29 @@ def lambda_handler(event: dict, context: LambdaContext):
             ExclusiveStartKey=query["LastEvaluatedKey"],
         )
         items.extend(query["Items"])
-    for item in items:
-        headers = {
-            "Content-Type": "application/json",
-            item["api_key_name"]: item["api_key_value"],
-        }
-        response = requests.post(
-            item["url"],
-            headers=headers,
-            json={
-                "event_timestamp": event.time,
-                "event_type": event.detail_type,
-                "event": event.detail,
-            },
-            timeout=30,
+    if len(items) > 0:
+        retries = Retry(
+            total=5,
+            backoff_factor=0.1,
+            status_forcelist=[500],
+            allowed_methods=["POST"],
         )
-        logger.info(f"Status Code: {response.status_code}")
+        s = Session()
+        for item in items:
+            headers = {
+                "Content-Type": "application/json",
+                item["api_key_name"]: item["api_key_value"],
+            }
+            s.mount(item["url"], HTTPAdapter(max_retries=retries))
+            response = s.post(
+                item["url"],
+                headers=headers,
+                json={
+                    "event_timestamp": event.time,
+                    "event_type": event.detail_type,
+                    "event": event.detail,
+                },
+                timeout=30,
+            )
+            logger.info(f"Status Code: {response.status_code}")
+            logger.info(response.text)
