212f060cf6759b06191fe32bf8ffb4cdd90923f0
Dan Johns <danjhd@amazon.com>
2025-02-07 20:52:39 +0000

    webhook update in preparation for PR in spec

--- functions/webhooks/app.py
+++ functions/webhooks/app.py
@@ -1,4 +1,6 @@
 import os
+from collections import defaultdict
+from functools import reduce
 
 import boto3
 from aws_lambda_powertools import Logger, Metrics, Tracer, single_metric
@@ -7,10 +9,11 @@ from aws_lambda_powertools.utilities.data_classes.event_bridge_event import (
     EventBridgeEvent,
 )
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import And, Attr, Key, Or
 from requests import Session
 from requests.adapters import HTTPAdapter, Retry
-from schema import Webhook
+from schema import Flow, Flowsegment, Source, Webhook
+from utils import generate_presigned_url, model_dump
 
 tracer = Tracer()
 logger = Logger()
@@ -18,61 +21,54 @@ metrics = Metrics(namespace="Powertools")
 
 dynamodb = boto3.resource("dynamodb")
 webhooks_table = dynamodb.Table(os.environ["WEBHOOKS_TABLE"])
+api_id = os.environ["API_ID"]
+stage_name = os.environ["STAGE_NAME"]
+bucket = os.environ["BUCKET"]
+bucket_region = os.environ["BUCKET_REGION"]
 
-event_mappings = {
-    "flows/created": ("flow_ids", ["flow", "id"]),
-    "flows/updated": ("flow_ids", ["flow", "id"]),
-    "flows/deleted": ("flow_ids", ["flow_id"]),
-    "flows/segments_added": ("flow_ids", ["flow_id"]),
-    "flows/segments_deleted": ("flow_ids", ["flow_id"]),
-    "sources/created": ("source_ids", ["source", "id"]),
-    "sources/updated": ("source_ids", ["source", "id"]),
-    "sources/deleted": ("source_ids", ["source_id"]),
+attribute_mappings = {
+    "flow": "flow_ids",
+    "source": "source_ids",
+    "flow-collected-by": "flow_collected_by_ids",
+    "source-collected-by": "source_collected_by_ids",
 }
 
-
-@tracer.capture_lambda_handler(capture_response=False)
-def get_event_key(event, keys):
-    response = event
-    for key in keys:
-        response = response.get(key, None)
-        if not response:
-            return None
-    return response
+store_name = (
+    boto3.client("apigateway")
+    .get_stage(restApiId=api_id, stageName=stage_name)["variables"]
+    .get("name", "example-store-name")
+)
 
 
-@logger.inject_lambda_context(log_event=True)
-@tracer.capture_lambda_handler(capture_response=False)
-@metrics.log_metrics(capture_cold_start_metric=True)
-# pylint: disable=unused-argument
-def lambda_handler(event: dict, context: LambdaContext):
-    event: EventBridgeEvent = EventBridgeEvent(event)
-    query = webhooks_table.query(
-        KeyConditionExpression=Key("event").eq(event.detail_type),
-    )
+@tracer.capture_method(capture_response=False)
+def get_matching_webhooks(event):
+    expressions = defaultdict(list)
+    for resource in event.resources:
+        _, resource_type, resource_id = resource.split(":")
+        expressions[attribute_mappings[resource_type]].append(resource_id)
+    filter_expressions = []
+    for attr, id_list in expressions.items():
+        id_expressions = [Attr(attr).contains(id_list[0])]
+        for r_id in id_list[1:]:
+            id_expressions.append(Attr(attr).contains(r_id))
+        id_expressions.append(Attr(attr).not_exists())
+        filter_expressions.append(reduce(Or, id_expressions))
+    args = {"KeyConditionExpression": Key("event").eq(event.detail_type)}
+    if len(filter_expressions) > 0:
+        args["FilterExpression"] = reduce(And, filter_expressions)
+    query = webhooks_table.query(**args)
     items = query["Items"]
     while "LastEvaluatedKey" in query:
         query = webhooks_table.query(
-            KeyConditionExpression=Key("event").eq(event.detail_type),
+            **args,
             ExclusiveStartKey=query["LastEvaluatedKey"],
         )
         items.extend(query["Items"])
-    schema_items = [Webhook(**item, events=[item["event"]]) for item in items]
-    for item in schema_items:
-        webhook_attr, event_keys = event_mappings[event.detail_type]
-        item_ids = getattr(item, webhook_attr, None)
-        event_id = get_event_key(event.detail, event_keys)
-        # Post event if no relevant filters are supplied
-        if not item_ids:
-            post_event(event, item)
-        # Post event if event_id is found within filters supplied
-        elif event_id in item_ids:
-            post_event(event, item)
-        else:
-            logger.info("No matching webhook definition found")
+    return [Webhook(**item, events=[item["event"]]) for item in items]
 
 
-def post_event(event, item):
+@tracer.capture_method(capture_response=False)
+def post_event(event, item, get_urls):
     retries = Retry(
         total=5,
         backoff_factor=0.1,
@@ -84,6 +80,17 @@ def post_event(event, item):
     if item.api_key_name and item.api_key_value:
         headers[item.api_key_name] = item.api_key_value
     s.mount(item.url, HTTPAdapter(max_retries=retries))
+    # Use associated model to clean the response data
+    match event.detail_type:
+        case "flows/created" | "flows/updated":
+            event.detail["flow"] = model_dump(Flow(**event.detail["flow"]))
+        case "source/created" | "source/updated":
+            event.detail["source"] = model_dump(Source(**event.detail["source"]))
+        case "flows/segments_added":
+            event.detail["segments"][0]["get_urls"] = get_urls
+            event.detail["segments"][0] = model_dump(
+                Flowsegment(**event.detail["segments"][0])
+            )
     response = s.post(
         item.url,
         headers=headers,
@@ -104,3 +111,63 @@ def post_event(event, item):
         metric.add_dimension(name="event_type", value=event.detail_type)
     logger.info(f"Status Code: {response.status_code}")
     logger.info(response.text)
+
+
+@logger.inject_lambda_context(log_event=True)
+@tracer.capture_lambda_handler(capture_response=False)
+@metrics.log_metrics(capture_cold_start_metric=True)
+# pylint: disable=unused-argument
+def lambda_handler(event: dict, context: LambdaContext):
+    event: EventBridgeEvent = EventBridgeEvent(event)
+    query = webhooks_table.query(
+        KeyConditionExpression=Key("event").eq(event.detail_type),
+    )
+    items = query["Items"]
+    while "LastEvaluatedKey" in query:
+        query = webhooks_table.query(
+            KeyConditionExpression=Key("event").eq(event.detail_type),
+            ExclusiveStartKey=query["LastEvaluatedKey"],
+        )
+        items.extend(query["Items"])
+    schema_items = get_matching_webhooks(event)
+    if event.detail_type == "flows/segments_added":
+        get_url = {
+            "label": f"aws.{bucket_region}:s3:{store_name}",
+            "url": f'https://{bucket}.s3.{bucket_region}.amazonaws.com/{event.detail["segments"][0]["object_id"]}',
+        }
+        get_urls = event.detail["segments"][0].get("get_urls", [])
+        get_urls.append(get_url)
+        need_presigned_urls = any(
+            item.accept_get_urls is None
+            or any(":s3.presigned:" in label for label in (item.accept_get_urls or []))
+            for item in schema_items
+        )
+        if need_presigned_urls:
+            get_urls.append(
+                {
+                    "label": f"aws.{bucket_region}:s3.presigned:{store_name}",
+                    "url": generate_presigned_url(
+                        "get_object", bucket, event.detail["segments"][0]["object_id"]
+                    ),
+                }
+            )
+    for item in schema_items:
+        if event.detail_type != "flows/segments_added":
+            post_event(event, item, None)
+        elif (
+            item.accept_get_urls is None
+        ):  # Explictly check for None since empty list is falsey
+            post_event(event, item, get_urls)
+        else:
+            # Only post the get_urls with matching label(s)
+            accepted_labels = set(item.accept_get_urls)
+            post_event(
+                event,
+                item,
+                list(
+                    filter(
+                        lambda u, accepted=accepted_labels: u["label"] in accepted,
+                        get_urls,
+                    )
+                ),
+            )
