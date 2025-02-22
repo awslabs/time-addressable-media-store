e4c7c738e80b44a2839af6a11cc8927bd7c278e1
Dan Johns <danjhd@amazon.com>
2025-02-22 13:29:50 +0000

    initial commit

--- functions/api_service/app.py
+++ functions/api_service/app.py
@@ -18,6 +18,7 @@ app = APIGatewayRestResolver(enable_validation=True, cors=CORSConfig())
 metrics = Metrics(namespace="Powertools")
 
 dynamodb = boto3.resource("dynamodb")
+agw = boto3.client("apigateway")
 table_name = os.environ.get("WEBHOOKS_TABLE", None)
 ssm = boto3.client("ssm")
 info_param_name = os.environ["SERVICE_INFO_PARAMETER"]
