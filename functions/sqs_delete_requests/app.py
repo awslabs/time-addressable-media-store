9497d8855a145481cf592d563e15b82c509843d9
Dan Johns <danjhd@amazon.com>
2025-04-16 13:52:20 +0100

    Implemented partial batch failure handling

--- functions/sqs_delete_requests/app.py
+++ functions/sqs_delete_requests/app.py
@@ -1,8 +1,14 @@
 import json
 import os
+from typing import Optional
 
 from aws_lambda_powertools import Logger, Metrics, Tracer
-from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSEvent
+from aws_lambda_powertools.utilities.batch import (
+    BatchProcessor,
+    EventType,
+    process_partial_response,
+)
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSEvent, SQSRecord
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from dynamodb import delete_flow_segments
 from mediatimestamp.immutable import TimeRange
@@ -17,46 +23,58 @@ from utils import publish_event
 tracer = Tracer()
 logger = Logger()
 metrics = Metrics()
+batch_processor = BatchProcessor(event_type=EventType.SQS)
 
 s3_queue = os.environ["S3_QUEUE_URL"]
 del_queue = os.environ["DELETE_QUEUE_URL"]
 
 
+@tracer.capture_method(capture_response=False)
+def record_handler(
+    record: SQSRecord, lambda_context: Optional[LambdaContext] = None
+) -> None:
+    """Processes a single SQS record"""
+    body = json.loads(record.body)
+    # If delete request has never been started and instructs flow deletion then delete the flow
+    if body["delete_flow"] and body["status"] == "created":
+        # Delete the flow
+        source_id = delete_flow(body["flow_id"])
+        if source_id:
+            publish_event(
+                "flows/deleted",
+                {"flow_id": body["flow_id"]},
+                enhance_resources([f'tams:flow:{body["flow_id"]}']),
+            )
+        # Delete source if no longer referenced by any other flows
+        if check_delete_source(source_id):
+            publish_event(
+                "sources/deleted",
+                {"source_id": source_id},
+                enhance_resources([f"tams:source:{source_id}"]),
+            )
+    # Now proceed with deleting the flow segments
+    body["status"] = "started"
+    set_node_property_base(
+        "delete_request", body["id"], {"delete_request.status": body["status"]}
+    )
+    delete_flow_segments(
+        body["flow_id"],
+        {"timerange": body["timerange_remaining"]},
+        TimeRange.from_str(body["timerange_remaining"]),
+        lambda_context,
+        s3_queue,
+        del_queue,
+        item_dict=body,
+    )
+
+
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler(capture_response=False)
 @metrics.log_metrics(capture_cold_start_metric=True)
-def lambda_handler(event: dict, context: LambdaContext) -> dict:
-    event: SQSEvent = SQSEvent(event)
-    for record in event.records:
-        body = json.loads(record.body)
-        # If delete request has never been started and instructs flow deletion then delete the flow
-        if body["delete_flow"] and body["status"] == "created":
-            # Delete the flow
-            source_id = delete_flow(body["flow_id"])
-            if source_id:
-                publish_event(
-                    "flows/deleted",
-                    {"flow_id": body["flow_id"]},
-                    enhance_resources([f'tams:flow:{body["flow_id"]}']),
-                )
-            # Delete source if no longer referenced by any other flows
-            if check_delete_source(source_id):
-                publish_event(
-                    "sources/deleted",
-                    {"source_id": source_id},
-                    enhance_resources([f"tams:source:{source_id}"]),
-                )
-        # Now proceed with deleting the flow segments
-        body["status"] = "started"
-        set_node_property_base(
-            "delete_request", body["id"], {"delete_request.status": body["status"]}
-        )
-        delete_flow_segments(
-            body["flow_id"],
-            {"timerange": body["timerange_remaining"]},
-            TimeRange.from_str(body["timerange_remaining"]),
-            context,
-            s3_queue,
-            del_queue,
-            item_dict=body,
-        )
+def lambda_handler(event: SQSEvent, context: LambdaContext) -> dict:
+    return process_partial_response(
+        event=event,
+        record_handler=record_handler,
+        processor=batch_processor,
+        context=context,
+    )
--- functions/sqs_object_cleanup/app.py
+++ functions/sqs_object_cleanup/app.py
@@ -4,7 +4,12 @@ from itertools import batched
 
 import boto3
 from aws_lambda_powertools import Logger, Metrics, Tracer
-from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSEvent
+from aws_lambda_powertools.utilities.batch import (
+    BatchProcessor,
+    EventType,
+    process_partial_response,
+)
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSEvent, SQSRecord
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from boto3.dynamodb.conditions import Key
 from dynamodb import segments_table
@@ -12,32 +17,42 @@ from dynamodb import segments_table
 tracer = Tracer()
 logger = Logger()
 metrics = Metrics()
+batch_processor = BatchProcessor(event_type=EventType.SQS)
 
 s3 = boto3.client("s3")
 bucket = os.environ["BUCKET"]
+
+
+@tracer.capture_method(capture_response=False)
+def record_handler(record: SQSRecord) -> None:
+    """Processes a single SQS record"""
+    delete_objects = []
+    object_ids = json.loads(record.body)
+    # Check if object_id of each deleted segment is still in use
+    for object_id in object_ids:
+        query = segments_table.query(
+            IndexName="object-id-index",
+            KeyConditionExpression=Key("object_id").eq(object_id),
+            Select="COUNT",
+        )
+        if query["Count"] == 0:
+            delete_objects.append({"Key": object_id})
+    # Delete orphan objects from S3
+    for delete_batch in batched(delete_objects, 1000):
+        s3.delete_objects(
+            Bucket=bucket,
+            Delete={"Objects": delete_batch},
+        )
 
 
 @logger.inject_lambda_context(log_event=True)
 @tracer.capture_lambda_handler(capture_response=False)
 @metrics.log_metrics(capture_cold_start_metric=True)
 # pylint: disable=unused-argument
-def lambda_handler(event: dict, context: LambdaContext) -> dict:
-    event: SQSEvent = SQSEvent(event)
-    for record in event.records:
-        delete_objects = []
-        object_ids = json.loads(record.body)
-        # Check if object_id of each deleted segment is still in use
-        for object_id in object_ids:
-            query = segments_table.query(
-                IndexName="object-id-index",
-                KeyConditionExpression=Key("object_id").eq(object_id),
-                Select="COUNT",
-            )
-            if query["Count"] == 0:
-                delete_objects.append({"Key": object_id})
-        # Delete orphan objects from S3
-        for delete_batch in batched(delete_objects, 1000):
-            s3.delete_objects(
-                Bucket=bucket,
-                Delete={"Objects": delete_batch},
-            )
+def lambda_handler(event: SQSEvent, context: LambdaContext) -> dict:
+    return process_partial_response(
+        event=event,
+        record_handler=record_handler,
+        processor=batch_processor,
+        context=context,
+    )
